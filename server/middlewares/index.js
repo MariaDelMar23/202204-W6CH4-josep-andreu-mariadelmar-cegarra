089b99616577c6c3f38b781fc443681f7455769a
josep andreu <josepa45@gmail.com>
2022-05-12 19:59:26 +0200

    Created error middlewares

---
+++ server/middlewares/index.js
@@ -0,0 +1,16 @@
+const debug = require("debug")("apiThings:middleware");
+
+// eslint-disable-next-line no-unused-vars
+const serverError = (error, req, res, next) => {
+  debug(error.message);
+  res.status(500).json({ msg: "Error del Servidor" });
+};
+
+const notFoundError = (req, res) => {
+  res.status(404).json({ msg: "Not Found" });
+};
+
+module.exports = {
+  serverError,
+  notFoundError,
+};
