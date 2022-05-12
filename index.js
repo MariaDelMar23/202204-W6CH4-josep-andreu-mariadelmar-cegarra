6db989c0caba604f600be7adc54cffac22404b79
MariaDelMar23 <maria.cegarra2303@gmail.com>
2022-05-12 19:24:09 +0200

    Open server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const a = 3;
+require("dotenv").config();
+const express = require("express");
+const debug = require("debug")("apiThings:root");
+
+const app = express();
+const server = app.listen(4000, () => {
+  debug("Server listening on port 4000");
+});
+
+server.on("error", (error) => {
+  debug(error);
+});
